e1f834e401dad18149d33e5dc3dd5e446c23c41e
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-27 22:30:42 +0530

    Submitted StringCompression

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,9 +1,30 @@
+import java.util.*;
 public class StringCompression {
-    
-        // TODO: Read the string
-        // TODO: Iterate through the string to count consecutive characters
-        // TODO: Append char and count to a StringBuilder
-        // TODO: Compare lengths of original vs compressed
-        // TODO: Print the shorter one (or compressed if equal length, depending on strict interpretation. Usually original if compressed is not strictly smaller, but prompt says "if compressed is longer")
-  
+        private static String compress(String s,int n){
+                StringBuilder sb = new StringBuilder();
+                int cnt = 1;
+                for(int i=1;i<n;i++){
+                        if(s.charAt(i-1)==s.charAt(i)){
+                                cnt++;
+                        }else{
+                                sb.append(s.charAt(i-1));
+                                sb.append(cnt);
+                                cnt=1;
+                        }
+                }
+                sb.append(s.charAt(n-1));
+                sb.append(cnt);
+                return sb.toString();
+        }
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                String s = sc.nextLine();
+                int n = s.length();
+                String res = compress(s,n);
+                if(res.length()>=n){
+                        System.out.println(s);
+                }else{
+                        System.out.println(res);
+                }
+        }
 }
